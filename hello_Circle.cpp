734b9ca1319ff36346b66fc8bb8a4f38e2907ccc
esc20936 <esc20936@uvg.edu.gt>
2023-09-01 11:58:20 -0600

    feat(): animations

--- hello_Circle.cpp
+++ hello_Circle.cpp
@@ -3,8 +3,10 @@
 #include <iostream>
 #include <cmath>
 #include <random>
+#include <ctime>
+#include <glm/glm.hpp>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
 
 // settings
@@ -12,11 +14,11 @@ const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 const char *vertexShaderSource = "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
+                                 "layout (location = 0) in vec3 aPos;\n"
+                                 "void main()\n"
+                                 "{\n"
+                                 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                 "}\0";
 const char *fragmentShaderSource = R"(
 #version 330 core
 out vec4 FragColor;
@@ -32,7 +34,7 @@ void main()
     float distance = length(fragPos - center);
 
     // Define bubble colors
-    vec3 bubbleColor = vec3(0.0, 0.5, 1.0); // Bubble color (blue)
+    vec3 bubbleColor = vec3(0.5, 0.5, 1.0); // Bubble color (blue)
 
     // Add a shimmering effect based on distance and time
     float shimmer = 0.1 * sin(distance * 20.0 + 2.0 * 3.14159265359 * gl_FragCoord.x / 800.0);
@@ -50,26 +52,25 @@ void main()
 }
 )";
 
-
-
-
-
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     // should read how many circles to draw from command line
-    if (argc != 2) {
+    if (argc != 2)
+    {
         std::cout << "Usage: " << argv[0] << " <number of circles>" << std::endl;
         return -1;
     }
 
     // validate the input is a number
-    
-    if(!isdigit(*argv[1])) {
+
+    if (!isdigit(*argv[1]))
+    {
         std::cout << "Usage: " << argv[0] << " <number of circles>" << std::endl;
         return -1;
     }
 
-    if(atoi(argv[1]) < 1) {
+    if (atoi(argv[1]) < 1)
+    {
         std::cout << "Usage: " << argv[0] << " <number of circles>" << std::endl;
         return -1;
     }
@@ -89,8 +90,9 @@ int main(int argc, char** argv)
 
     // glfw window creation
     // --------------------
-    GLFWwindow* window = glfwCreateWindow(1920, 1080, "Fullscreen OpenGL", glfwGetPrimaryMonitor(), NULL);
-    if (window == NULL) {
+    GLFWwindow *window = glfwCreateWindow(1920, 1080, "Fullscreen OpenGL", glfwGetPrimaryMonitor(), NULL);
+    if (window == NULL)
+    {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
@@ -122,7 +124,8 @@ int main(int argc, char** argv)
     if (!success)
     {
         glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+                  << infoLog << std::endl;
     }
     // fragment shader
     unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
@@ -133,7 +136,8 @@ int main(int argc, char** argv)
     if (!success)
     {
         glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
+                  << infoLog << std::endl;
     }
     // link shaders
     unsigned int shaderProgram = glCreateProgram();
@@ -142,9 +146,11 @@ int main(int argc, char** argv)
     glLinkProgram(shaderProgram);
     // check for linking errors
     glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
+    if (!success)
+    {
         glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n"
+                  << infoLog << std::endl;
     }
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
@@ -154,30 +160,30 @@ int main(int argc, char** argv)
     const float radius = 0.10f;
     const int segments = 360; // Number of triangle fan segments
 
-    
-    
+    glm::vec2 *circlePositions = new glm::vec2[numCircles];
 
     const int spaceForVertices = 3 * (segments + 2); // (x, y, z) for each vertex
 
     // Calculate vertices for the circle
-    float* vertices = new float[spaceForVertices * numCircles]; // (x, y, z) for each vertex
+    float *vertices = new float[spaceForVertices * numCircles]; // (x, y, z) for each vertex
 
-    for (int circle = 0; circle < numCircles; circle++) {
+    for (int circle = 0; circle < numCircles; circle++)
+    {
         float theta = 1.50f * 3.1415926f * float(circle) / float(numCircles);
         int offset = spaceForVertices * circle; // Start from index 3
 
-        // float centerX = 
-        // float centerY = 2.0f;
-        // float centerX should be a random number between -1.0 and 1.0
-        // float centerY should be a random number between -1.0 and 1.0
         float centerX = 2.0f * (float)rand() / (float)RAND_MAX - 1.0f;
         float centerY = 2.0f * (float)rand() / (float)RAND_MAX - 1.0f;
+
+        circlePositions[circle].x = centerX;
+        circlePositions[circle].y = centerY;
 
         vertices[offset] = centerX;
         vertices[offset + 1] = centerY;
         vertices[offset + 2] = 0.0f;
 
-        for (int i = 0; i <= segments; ++i) {
+        for (int i = 0; i <= segments; ++i)
+        {
             float theta = 2.0f * 3.1415926f * float(i) / float(segments);
             int offset = spaceForVertices * circle + 3 * (i + 1); // Start from index 3
 
@@ -196,7 +202,7 @@ int main(int argc, char** argv)
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(float) * spaceForVertices * numCircles, vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
 
     // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
@@ -204,23 +210,79 @@ int main(int argc, char** argv)
 
     // render loop
     // -----------
-    while (!glfwWindowShouldClose(window))
+    std::vector<glm::vec2> circleSpeeds(numCircles);
+
+    // Initialize speeds for each circle (you can set different initial speeds)
+    for (int circle = 0; circle < numCircles; circle++)
     {
-        processInput(window);
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        float initialSpeedX = 0.0005f;
+        float initialSpeedY = 0.0005f;
+        circleSpeeds[circle] = glm::vec2(initialSpeedX, initialSpeedY);
+    }
+    while (!glfwWindowShouldClose(window))
+{
+    processInput(window);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
+    // Update the time for animation
+    float time = glfwGetTime();
 
-        for (int circle = 0; circle < numCircles; circle++)
+    glUseProgram(shaderProgram);
+    glUniform1f(glGetUniformLocation(shaderProgram, "time"), time); // Pass time to the shader
+
+    glBindVertexArray(VAO);
+
+    // Update circle positions and buffer data
+    for (int circle = 0; circle < numCircles; circle++)
+    {
+        circlePositions[circle].x += circleSpeeds[circle].x; // Adjust the movement speed as needed
+        circlePositions[circle].y += circleSpeeds[circle].y; // Adjust the movement speed as needed
+
+        // Check if the circle reaches the screen boundaries
+        if (circlePositions[circle].x > 1.0f - radius || circlePositions[circle].x < -1.0f + radius)
         {
-            glDrawArrays(GL_TRIANGLE_FAN, circle * (segments + 2), segments + 2);
+            // Reverse the x-direction to simulate bounce
+            circleSpeeds[circle].x *= -1.0f;
+        }
+        if (circlePositions[circle].y > 1.0f - radius || circlePositions[circle].y < -1.0f + radius)
+        {
+            // Reverse the y-direction to simulate bounce
+            circleSpeeds[circle].y *= -1.0f;
         }
 
-        glfwSwapBuffers(window);
-        glfwPollEvents();
+        // Update the buffer data with the new positions
+        int offset = spaceForVertices * circle;
+        vertices[offset] = circlePositions[circle].x;
+        vertices[offset + 1] = circlePositions[circle].y;
+        vertices[offset + 2] = 0.0f;
+
+        for (int i = 0; i <= segments; ++i)
+        {
+            float theta = 2.0f * 3.1415926f * float(i) / float(segments);
+            int offset = spaceForVertices * circle + 3 * (i + 1); // Start from index 3
+
+            vertices[offset] = circlePositions[circle].x + radius * cos(theta);
+            vertices[offset + 1] = circlePositions[circle].y + radius * sin(theta);
+            vertices[offset + 2] = 0.0f;
+        }
     }
+
+    // Update the buffer data
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * spaceForVertices * numCircles, vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // Render circles
+    for (int circle = 0; circle < numCircles; circle++)
+    {
+        glDrawArrays(GL_TRIANGLE_FAN, circle * (segments + 2), segments + 2);
+    }
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+}
+
 
     // optional: de-allocate all resources once they've outlived their purpose:
     // ------------------------------------------------------------------------
@@ -246,9 +308,9 @@ void processInput(GLFWwindow *window)
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
 // ---------------------------------------------------------------------------------------------
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 {
-    // make sure the viewport matches the new window dimensions; note that width and 
+    // make sure the viewport matches the new window dimensions; note that width and
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
 }
